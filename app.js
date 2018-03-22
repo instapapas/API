209b1b257a767c0ddf4e0e7e633518af73fec813
Matías con tilde <saitam.spatz@gmail.com>
2018-03-22 22:46:52 +0100

    Refractoring mostly. And also some stuff is a bit better I think maybe

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const multer = require('multer');
-const upload = multer({ dest: 'files' });
+const upload = multer({
+  dest: 'files'
+});
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(cors());
 
 const authenticate = (req, res, next) => {
   if (req.body.userId != 'undefined') {
-    users.findOne(req.body.userId)
-    .then(data => {
+    users.findOne(req.body.userId).then(data => {
       if (data._authToken === req.body.authToken) {
         if (new Date().getTime() < data._sessionExpire) {
           next();
@@ -50,24 +53,25 @@ const config = require('./config.js');
 app.post('/sign-up', (req, res) => {
   users.findOne({
     username: req.body.username
-  })
-  .then(data => {
-    if (data) {
+  }).then(data => {
+    if (req.body.password != req.body.confirmPassword) {
       res.json(false);
     } else {
-      bcrypt.hash(req.body.password, 12)
-      .then(hash => {
-        const user = {
-          username: req.body.username,
-          password: hash,
-          email: req.body.email,
-          name: req.body.name
-        };
-        users.insert(user)
-        .then(inserted => {
-          res.json(true);
+      if (data) {
+        res.json(false);
+      } else {
+        bcrypt.hash(req.body.password, 12).then(hash => {
+          const user = {
+            username: req.body.username,
+            password: hash,
+            email: req.body.email,
+            name: req.body.name
+          };
+          users.insert(user).then(inserted => {
+            res.json(true);
+          });
         });
-      });
+      }
     }
   });
 });
@@ -75,11 +79,9 @@ app.post('/sign-up', (req, res) => {
 app.post('/log-in', (req, res) => {
   users.findOne({
     username: req.body.username
-  })
-  .then(data => {
+  }).then(data => {
     if (data) {
-      bcrypt.compare(req.body.password, data.password)
-      .then(same => {
+      bcrypt.compare(req.body.password, data.password).then(same => {
         if (same) {
           let randStr = '';
           for (let i = 0; i < config.AUTH_TOKEN_LENGTH; i++) {
@@ -91,8 +93,7 @@ app.post('/log-in', (req, res) => {
               _authToken: randStr,
               _sessionExpire: new Date().getTime() + config.LOGIN_MAX_TIME
             }
-          })
-          .then(updated => {
+          }).then(updated => {
             res.json({
               userId: data._id,
               _authToken: randStr
@@ -108,69 +109,70 @@ app.post('/log-in', (req, res) => {
   });
 });
 
-app.post('/upload-image', authenticate, upload.single('file'), (req, res) => {
+app.post('/upload-image', upload.single('file'), authenticate, (req, res) => {
   const image = {
     userId: req.body.userId,
     name: req.body.name,
-    upvotes: 0,
-    downvotes: 0,
+    upvotes: [],
+    downvotes: [],
     comments: [],
     _path: req.file.path
   };
 
-  images.insert(image)
-  .then(data => {
-    images.update(data._id, {
-      $set: {
-        url: config.IMAGE_URL + data._id
-      }
-    })
-    .then(updated => {
-      res.json(true);
-    });
+  images.insert(image).then(data => {
+    res.json(true);
   });
 });
 
+
 app.post('/vote', authenticate, (req, res) => {
-  images.findOne(req.body.imageId)
-  .then(data => {
-    data[`${req.body.type}votes`]++;
-    images.update(data._id, {
-      $set: data
-    })
-    .then(updated => {
-      res.json(true);
-    });
+  images.findOne(req.body.imageId).then(data => {
+    const toggle = (array, val) => {
+      const index = array.indexOf(val);
+      if (index === -1) array.push(val);
+      else array.splice(index, 1);
+      return array;
+    };
+
+    if (req.body.type === 'up') {
+      images.update(data._id, {
+        $set: {
+          upvotes: toggle(data.upvotes, req.body.userId)
+        }
+      }).then(updated => {
+        res.json(data.upvotes.length);
+      });
+    } else if (req.body.type === 'down') {
+      images.update(data._id, {
+        $set: {
+          downvotes: toggle(data.downvotes, req.body.userId)
+        }
+      }).then(updated => {
+        res.json(data.downvotes.length);
+      });
+    } else {
+      res.json(false);
+    }
   });
 });
 
 app.post('/comment', authenticate, (req, res) => {
-  images.findOne(req.body.image)
-  .then(data => {
+  images.findOne(req.body.imageId).then(data => {
     data.comments.push({
       userId: req.body.userId,
       text: req.body.text
     });
+
     images.update(data._id, {
       $set: data
-    })
-    .then(updated => {
-      res.json(true);
+    }).then(updated => {
+      res.json(data.comments.length);
     });
   });
 });
 
-// Serve image file
-app.get('/img/:imageId', (req, res) => {
-  images.findOne(req.params.imageId)
-  .then(data => {
-    res.sendFile(`${__dirname}/${data._path}`);
-  });
-});
-
 app.get('/get-user', (req, res) => {
-  users.findOne(req.query.userId)
-  .then(data => {
+  users.findOne(req.query.userId).then(data => {
     res.json({
       userId: data._id,
       username: data.username,
@@ -180,82 +182,77 @@ app.get('/get-user', (req, res) => {
 });
 
 app.get('/get-image', (req, res) => {
-  images.findOne(req.query.imageId)
-  .then(data => {
+  images.findOne(req.query.imageId).then(data => {
     res.json({
       imageId: data._id,
-      url: data.url,
+      userId: data.userId,
       name: data.name,
-      upvotes: data.upvotes,
-      downvotes: data.downvotes
+      upvotes: data.upvotes.length,
+      downvotes: data.downvotes.length,
+      comments: data.comments.length
     });
   });
 });
 
-app.get('/search', (req, res) => {
-  images.find()
-  .then(data => {
-    let names = [];
-    for (image of data) {
-      names.push(image.name);
-    }
-
-    const search = wade(names)(req.query.name);
-
-    let results = [];
-    for (index of search) {
-      const image = data[index.index];
-      results.push({
-        imageId: image._id,
-        url: image.url,
-        name: image.name,
-        upvotes: image.upvotes,
-        downvotes: image.downvotes,
-        comments: []
-      });
-    }
-
-    res.json(results);
+app.get('/all-users', (req, res) => {
+  users.find().then(data => {
+    res.json(data.map(user => {
+      return {
+        userId: user._id,
+        username: user.username,
+        name: user.name
+      };
+    }));
   });
 });
 
 app.get('/all-images', (req, res) => {
-  images.find()
-  .then(data => {
-    let results = [];
-    for (image of data) {
-      results.push({
+  images.find().then(data => {
+    res.json(data.map(image => {
+      return {
         imageId: image._id,
-        url: image.url,
+        userId: image.userId,
         name: image.name,
-        upvotes: image.upvotes,
-        downvotes: image.downvotes,
-        comments: []
-      });
-    }
-    res.json(results);
+        upvotes: image.upvotes.length,
+        downvotes: image.downvotes.length,
+        comments: image.comments.length
+      };
+    }));
+  });
+});
+
+app.get('/search', (req, res) => {
+  images.find().then(data => {
+    const search = wade(data.map(image => image.name));
+    const results = search(req.query.name);
+
+    res.json(results.map(index => {
+      return {
+        imageId: data[index.index]._id,
+        userId: data[index.index].userId,
+        name: data[index.index].name,
+        upvotes: data[index.index].upvotes.length,
+        downvotes: data[index.index].downvotes.length,
+        comments: data[index.index].comments.length
+      };
+    }));
   });
 });
 
 app.get('/get-comments', (req, res) => {
-  images.findOne(req.query.imageId)
-  .then(data => {
-    res.json(data.comments.splice(0, req.query.amount));
+  images.findOne(req.query.imageId).then(data => {
+    if (req.query.amount > 0) {
+      res.json(data.comments.slice(0, req.query.amount));
+    } else {
+      res.json(data.comments);
+    }
   });
 });
 
-app.get('/all-users', (req, res) => {
-  users.find()
-  .then(data => {
-    let results = [];
-    for (user of data) {
-      results.push({
-        userId: user._id,
-        username: user.username,
-        name: user.name
-      });
-    }
-    res.json(results);
+// Serve image file
+app.get('/img/:imageId', (req, res) => {
+  images.findOne(req.params.imageId).then(data => {
+    res.sendFile(`${__dirname}/${data._path}`);
   });
 });
 
